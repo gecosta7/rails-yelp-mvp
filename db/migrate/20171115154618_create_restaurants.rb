94a75488c745f594a35f82f306dfad059edd5a18
Geraldo Costa <gecosta7@gmail.com>
2017-11-15 14:05:58 -0200

    Add restaurant model

---
+++ db/migrate/20171115154618_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_nummber
+      t.integer :stars
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,81 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
+    stars:        3
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+    stars:        1
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
+    stars:        3
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+rails db:seed
+BEYOND CRUD
+
+You are not limited to the seven routes that RESTful routing creates by default.
+Say you want a route to list three stars restaurants. Something like:
+GET /restaurants/top
+NESTED RESOURCES
+
+Now we want routes to add a review on a restaurant
+GET /restaurants/42/reviews/new
+POST /restaurants/42/reviews
+NAMESPACED ROUTING
+
+VALIDATION
+
+# app/models/restaurant.rb
+class Restaurant < ApplicationRecord
+  validates :stars, inclusion: { in: [1, 2, 3], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+end
+# app/controllers/restaurants_controller.rb
+class RestaurantsController < ApplicationController
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # Unless @restaurant.valid?, #save will return false,
+    # and @restaurant is not persisted.
+    # TODO: present the form again with error messages.
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+end
+# app/controllers/restaurants_controller.rb
+class RestaurantsController < ApplicationController
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+end
